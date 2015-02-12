9c649db63c028434691267abaa07e1500bfeb847
Manraj Singh <manrajsinghgrover@gmail.com>
2015-02-12 13:15:15 +0530

    Reduced code size
    Reduced code from 22 lines to 6 lines

--- ConsoleCode.js
+++ ConsoleCode.js
@@ -1,6 +1,6 @@
-var input=$('.link');
-var output=[];
+var input=document.getElementsByClassName('link');
 for (var i = 0; i < input.length; i++){
-    var len=input[i].length;
-    output.push(input[i].getAttribute('href').slice(1,len));
+	var len=input[i].length;
+	var sliced=input[i].getAttribute('href').slice(1,len);
+	window.open("http://ideone.com/plain/"+sliced);
 }
