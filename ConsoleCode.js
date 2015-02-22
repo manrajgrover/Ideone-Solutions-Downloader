a065aaa990b56e6b8fc378b1428370a78d70e5ea
Manraj Singh <manrajsinghgrover@gmail.com>
2015-02-22 21:59:59 +0530

    Automated for full code base.
    Full Code base download. No need to copy paste on each page manually
    now.

--- ConsoleCode.js
+++ ConsoleCode.js
@@ -1,6 +1,18 @@
-var input=document.getElementsByClassName('link');
-for (var i = 0; i < input.length; i++){
-	var len=input[i].length;
-	var sliced=input[i].getAttribute('href').slice(1,len);
-	window.open("http://ideone.com/plain/"+sliced);
-}
+var list=$('ul[class="dropdown-menu "]')[0].children.length;
+var download=function(){
+	if(list>0){
+		list--;
+		var input=$('.link');
+		for(var i=0;i<input.length;i++){
+			var len=input[i].length;
+			var sliced=input[i].getAttribute('href').slice(1,len);
+			window.open("http://ideone.com/plain/"+sliced);
+			setTimeout(function(){console.log("Testing: "+i);},1000);
+		}
+		$('.icon-angle-right').click();
+	}
+	else{
+		clearInterval(download);
+	}
+};
+setInterval(download,10000);
